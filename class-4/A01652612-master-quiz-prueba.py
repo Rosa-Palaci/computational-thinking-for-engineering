bcbf79998d32af9d5c281b0bc3d79154027e3692
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-01 16:36:47 -0500

    Ejemplo con 3 and

--- class-4/A01652612-master-quiz-prueba.py
+++ class-4/A01652612-master-quiz-prueba.py
@@ -1,13 +1,15 @@
 a = 4
 b = 10
+c = 5
 
-if a == 4 or b == 10:
+if a == 4 and b == 10 and c == 5:
     print('M1')
 else:
     print('M2')
 
-#True M1
-#False M2
+#True print M1
+#False print M2
 #And es mas estricto que or
-#a==4 True 
+#a==4 True a=!4 False
 #b==10 True
+#c==5 True
