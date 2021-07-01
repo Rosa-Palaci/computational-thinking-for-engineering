64dc2b6b18ce96240a4caa83a08920a70ab4c8eb
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-01 18:47:57 -0500

    Actividad tipos de triangulo programado

--- tarea-semana-1/A01652612-tipos-de-triangulos.py
+++ tarea-semana-1/A01652612-tipos-de-triangulos.py
@@ -2,38 +2,54 @@
 #Rosa Vanessa Palacios Beltran
 #A01652612
 
-#Escribe un programa que dados 3 números enteros, 
-#que representan la longitud de los lados de un triángulo, 
-#muestre en la pantalla el tipo de triángulo del que se trata 
-#(equilatero, isósceles o escaleno).
+lado_x = int(input('Ingrese valor para lado x: '))
+lado_y = int(input('Ingrese valor para lado y: '))
+lado_z = int(input('Ingrese valor para lado z: '))
 
-#Considera que X, Y y Z son los lados de un triángulo 
-#si cumplen con las siguientes condiciones:
-#Todos los números son mayores que cero
-#X + Y > Z
-#X + Z > Y
-#Y + Z > X
+if (lado_x, lado_y, lado_z) == 0:
+    print('Valores no validos')
+    quit()
+else:
+    if (lado_x + lado_y) > lado_z:
+        print
+    else:
+        print('Valores no validos')
+        quit()
+    if (lado_x + lado_z) > lado_y:
+        print
+    else:
+        print('Valores no validos')
+        quit()
+    if (lado_y + lado_z) > lado_x:
+        print
+    else:
+        print('Valores no validos')
+        quit()
 
-#Es decir, la suma de dos de las medidas debe ser 
-#estrictamente mayor que la tercera.   
+print(f'Los valores de (x, y, z) son: {lado_x, lado_y, lado_z}')
 
-#Recuerda que el triángulo equilátero tiene 3 lados iguales, 
-#el isósceles tiene 2 lados iguales y 
-#el escaleno tiene los 3 lados diferentes.
+if lado_x == lado_y == lado_z:
+    tipo = 'equilatero'#el triángulo equilátero tiene 3 lados iguales
+elif lado_x == lado_y:
+    tipo = 'isósceles'#el isósceles tiene 2 lados iguales 
+elif lado_x == lado_z:
+    tipo = 'isósceles'
+elif lado_y == lado_z:
+    tipo = 'isósceles'
+elif lado_x != lado_y != lado_z:
+    tipo = 'escaleno'#el escaleno tiene los 3 lados diferentes
+else:
+    print('No existe')
+    quit()
 
-#Ejecuta tu programa con los siguientes casos de prueba
+print(f'El tipo de triángulo es {tipo}')
 
-"""
-caso1: X = 3, Y = 3, Z = 3
-caso2: X = 1, Y = 2, Z = 2
-caso3: X = 2, Y = 1, Z = 2
-caso4: X  = 2, Y = 2, Z = 1
-caso5: X = 7, Y = 5, Z = 10
-caso6: X = 10, Y = 7, Z = 5
-caso7: X = 1, Y = 2, Z = 3 
-"""
+   
 
-
-
-
-
+#caso1: X = 3, Y = 3, Z = 3
+#caso2: X = 1, Y = 2, Z = 2
+#caso3: X = 2, Y = 1, Z = 2
+#caso4: X  = 2, Y = 2, Z = 1
+#caso5: X = 7, Y = 5, Z = 10
+#caso6: X = 10, Y = 7, Z = 5
+#caso7: X = 1, Y = 2, Z = 3 
