c81ae53c9fffd367218b5455c702298918dca79b
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-01 16:27:38 -0500

    Programa ejercio del if y and

---
+++ class-4/A01652612.py
@@ -0,0 +1,16 @@
+#Ejercicio 3 en clase 4 - 01/07/21
+#Rosa Vanessa Palacios Beltran
+#A01652612
+
+#Realiza un program que verifique el valor
+#de dos variables 'x' y 'y'
+#Si 'x' es 2 y 'y' es 3, imprime 'Mensaje 1'
+#En caso contrario, imprime 'Mensaje 2'
+
+x = int(input('x = '))
+y = int(input('y = '))
+
+if x == 2 and y == 3:
+    print('Mensaje 1')
+else:
+    print('Mensaje 2')
