226d5562776bc4882e2b4329c09a51bf30388245
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-01 17:13:28 -0500

    Se agrego carpeta de tarea y actividad de tipos de triangulos

---
+++ tarea-semana-1/A01652612-tipos-de-triangulos.py
@@ -0,0 +1,39 @@
+#Actividad en clase - Estatutos de decisión - 1
+#Rosa Vanessa Palacios Beltran
+#A01652612
+
+#Escribe un programa que dados 3 números enteros, 
+#que representan la longitud de los lados de un triángulo, 
+#muestre en la pantalla el tipo de triángulo del que se trata 
+#(equilatero, isósceles o escaleno).
+
+#Considera que X, Y y Z son los lados de un triángulo 
+#si cumplen con las siguientes condiciones:
+#Todos los números son mayores que cero
+#X + Y > Z
+#X + Z > Y
+#Y + Z > X
+
+#Es decir, la suma de dos de las medidas debe ser 
+#estrictamente mayor que la tercera.   
+
+#Recuerda que el triángulo equilátero tiene 3 lados iguales, 
+#el isósceles tiene 2 lados iguales y 
+#el escaleno tiene los 3 lados diferentes.
+
+#Ejecuta tu programa con los siguientes casos de prueba
+
+"""
+caso1: X = 3, Y = 3, Z = 3
+caso2: X = 1, Y = 2, Z = 2
+caso3: X = 2, Y = 1, Z = 2
+caso4: X  = 2, Y = 2, Z = 1
+caso5: X = 7, Y = 5, Z = 10
+caso6: X = 10, Y = 7, Z = 5
+caso7: X = 1, Y = 2, Z = 3 
+"""
+
+
+
+
+
