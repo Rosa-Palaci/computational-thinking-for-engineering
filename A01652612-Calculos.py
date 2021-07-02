00a6ea3162b1d8f8c39b11d7a960e7e460ffd51d
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-01 20:55:40 -0500

    Se programo calculos.py

--- A01652612-Calculos.py
+++ A01652612-Calculos.py
@@ -6,3 +6,20 @@
 #Realiza las siguientes operaciones en un programa de Python
 
 import math
+
+a = 4
+b = 5
+
+print(f'Valor de a: {a}')
+print(f'Valor de b: {b}')
+
+
+oper3 = ((a**3)+(2*(b**2))) / ((2*((a+b)**2)) + (4*((a-b)**2)))
+
+oper4 = ((2*((a+b)**2)) + (4*((a-b)**2))) / ((a*b)**2)
+
+oper5 = ((math.sqrt((a+b)**2) + (2**(a+b))) / ( ( (2*a) + (2*b) )**2 ) )
+
+print(f'El resultado de oper3 es: {oper3}')
+print(f'El resultado de oper4 es: {oper4}')
+print(f'El resultado de oper5 es: {oper5}')
