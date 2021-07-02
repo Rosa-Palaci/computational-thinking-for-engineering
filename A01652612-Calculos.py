53cfa545f759fdb7a2e05a68f5d8735f76281b4f
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-01 20:37:39 -0500

    Se creo archivo calculos.py

---
+++ A01652612-Calculos.py
@@ -0,0 +1,8 @@
+#Actividad - Programas que usan estatutos de decisión con operaciones aritméticas
+#Calculos
+#Rosa Vanessa Palacios Beltran
+#A01652612
+
+#Realiza las siguientes operaciones en un programa de Python
+
+import math
