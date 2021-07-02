43026c876a769e35c2ef1f9dcadae19a2ac896b0
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-01 20:59:26 -0500

    Se creo distancia.py

---
+++ tarea-semana-1/A01652612-distancia.py
@@ -0,0 +1,8 @@
+#Actividad - Programas que usan estatutos de decisión con operaciones aritméticas
+#Distancia
+#Rosa Vanessa Palacios Beltran
+#A01652612
+
+#Realiza las siguientes operaciones en un programa 
+#la distancia entre dos puntos del plano cartesiano.
+
