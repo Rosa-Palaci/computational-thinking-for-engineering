121d08967743e1bd99bb168925ea11d19df1e6a0
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-06 14:46:40 -0500

    Apunte para la clase 6 - estudio previo de estatuto for y los rangos

--- semana-2/A01652612-ejercicio-2.py
+++ semana-2/A01652612-ejercicio-2.py
@@ -4,25 +4,41 @@
 
 #Ejericicio # 2
 
-# La fórmula para sacar la distancia entre dos puntos es:
-# ditancia = raiz cuadrada ((x2 - x1)**2 + (y2- y1)**2)
-
 import math
 
+print('Acontinuación ingresa el punto centro del círculo')
+
+x1 = float(input("punto centro de x: "))
+y1 = float(input("punto centro de y: "))
+puntoCentro = str(input(f'Coordenadas del centro del círculo: ({x1},{y1})'))
+
 radio = float(input('Ingresar el radio del círculo: '))
-puntoCentro = float(input('Ingresar coordenadas del centro del círculo: '))
-punto = float(input('Ingresar coordenadas de un punto distinto al centro del círculo: '))
 
-if puntoCentro <= 0:
-    print('Coordenada no valida, intente otra forma.')
+if radio < x1:
+    print('Error')
     quit()
-elif puntoCentro == 1:
-    print(f'El {puntoCentro} se encuentra dentro del círculo')
+elif radio < y1:
+    print('Error')
+    quit()
 else:
-    print(f'El {puntoCentro} se enccuentra fuera del círculo')
+    print('Elige un punto diferente o igual al centro del círculo')
 
-circunferancia = input('dentro,fuera o sobre la circunfernacia')
+x2 = float(input("punto de x: "))
+y2 = float(input("punto de y: "))
+punto = str(input(f'Coordenadas del punto elegido son:({x2},{y2})'))
 
 distancia = math.sqrt( ( (x2 - x1)**2 ) + ( (y2-y1)**2 ) )
 
-print(f'La distancia entre los puntos es de: {distancia:.2f} por lo tanto el punto esta {circunferancia}')
+print(f'La distancia entre los puntos es de: {distancia:.2f}')
+
+if ({x2},{y2}) >= ({x1},{y1}):
+    circunferancia = print(f'El punto en ({x2},{y2}) se encuentra fuera del círculo')
+    ubicacion = 'fuera de la circunferencia' 
+elif ({x2},{y2}) <= ({x1},{y1}):
+    circunferancia = print(f'El punto en ({x2},{y2}) se encuentra dentro del círculo')
+    ubicacion = 'dentro de la circunferencia' 
+elif ({x2},{y2}) == ({x1},{y1}):
+    circunferancia = print(f'El punto en ({x2},{y2}) se encuentra sobre la circunferencia')
+    ubicacion = 'sobre de la circunferencia' 
+
+print(f'Por lo tanto el punto elegido esta {ubicacion}')
