1cb1c79666ed7dea7b05f0b5ec4c5b8291c0ac6b
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-06 16:45:07 -0500

    Mi forma de resolver la actividad de estatus for

--- semana-2/class-6/A01652612-EstatutosFor.py
+++ semana-2/class-6/A01652612-EstatutosFor.py
@@ -2,9 +2,28 @@
 #Rosa Vanessa Palacios Beltran
 #A01652612
 
-#Instrucciones
-#Escribe un programa que lea un número entero positivo n 
-# y muestre una lista de números que empieza en 1 e 
-# incrementa de uno en uno hasta llegar a n y después 
-# decrementa de uno en uno hasta llegar a 1:
+#Instrucciones 
+'''
+Escribe un programa que lea un número entero positivo n 
+ y muestre una lista de números que empieza en 1 e 
+ incrementa de uno en uno hasta llegar a n y después 
+ decrementa de uno en uno hasta llegar a 1:
 # 1, 2, ... n, n-1, n-2 ... 1
+'''
+
+#numEntero 
+
+n = int(input('Digite un número entero pisitivo: '))
+valorInicial = 1
+
+print(f'\n Lista de ({valorInicial} a {n}) de uno en uno')
+
+for listaCrece in range(valorInicial,n + 1,1):
+    print(listaCrece, end = ', ')
+
+valorFinal = 1
+
+print(f'\n Lista de ({n} a {valorFinal}) de menos uno en menos uno')
+if n > valorFinal:
+    for listaDecrece in range(n,valorFinal-1,-1):
+        print(listaDecrece, end = ', ')
