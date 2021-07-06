ae7dbe9a7628dd35063dad89f163e5ea7d0ff11b
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-06 15:25:54 -0500

    Tarea números ascendentes de una forma rudimentaria

---
+++ semana-2/class-6/A01652612-EstatutosFor.py
@@ -0,0 +1,10 @@
+#Actividad en clase - Estatuto for y los rangos - 1
+#Rosa Vanessa Palacios Beltran
+#A01652612
+
+#Instrucciones
+#Escribe un programa que lea un número entero positivo n 
+# y muestre una lista de números que empieza en 1 e 
+# incrementa de uno en uno hasta llegar a n y después 
+# decrementa de uno en uno hasta llegar a 1:
+# 1, 2, ... n, n-1, n-2 ... 1
--- semana-2/class-6/A01652612-class-6.py
+++ semana-2/class-6/A01652612-class-6.py
@@ -2,4 +2,44 @@
 #Rosa Vanessa Palacios Beltran
 #A01652612
 
-#Instrucciones
+#Tarea de clase 5
+
+import random
+
+'''a = int(input('Número a: '))
+b = int(input('Número b: '))
+c = int(input('Número c: '))'''
+
+a = random.radint(0,20)
+b = random.radint(0,20)
+c = random.radint(0,20)
+
+print(f'Los númros sin ordenar son: {a}, {b} y {c}\n')
+
+
+if a > b and a > c:
+    mayor = a
+    if b > c:
+        medio = b
+        menor = c
+    else: # significa que c > b
+        medio = c
+        menor = b
+elif b > a and b > c:
+    mayor = b
+    if a > c:
+        medio = a
+        menor = c
+    else: 
+        medio = c
+        menor = a
+else: #Significa que 'c' es el mayor
+    mayor = c
+    if a > b:
+        medio = a
+        menor = b
+    else: 
+        medio = b
+        menor = a
+
+print(f'Los números en orden ascendente son: {menor}, {medio}, {mayor}')
