2a785667d850de31b0f6240828ec40d952c6a9ea
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-06 17:27:03 -0500

    EstatusFor trabaja correctamente, según las instrucciónes, son 18 lineas de código funcional

--- semana-2/class-6/A01652612-EstatutosFor.py
+++ semana-2/class-6/A01652612-EstatutosFor.py
@@ -1,29 +1,29 @@
 #Actividad en clase - Estatuto for y los rangos - 1
 #Rosa Vanessa Palacios Beltran
 #A01652612
-
-#Instrucciones 
 '''
+Instrucciones 
 Escribe un programa que lea un número entero positivo n 
  y muestre una lista de números que empieza en 1 e 
  incrementa de uno en uno hasta llegar a n y después 
  decrementa de uno en uno hasta llegar a 1:
 # 1, 2, ... n, n-1, n-2 ... 1
 '''
-
-#numEntero 
-
 n = int(input('Digite un número entero pisitivo: '))
 valorInicial = 1
 
-print(f'\n Lista de ({valorInicial} a {n}) de uno en uno')
+print(f'Lista de ({valorInicial} a {n}) de uno en uno')
 
 for listaCrece in range(valorInicial,n + 1,1):
-    print(listaCrece, end = ', ')
-
-valorFinal = 1
-
-print(f'\n Lista de ({n} a {valorFinal}) de menos uno en menos uno')
-if n > valorFinal:
-    for listaDecrece in range(n,valorFinal-1,-1):
-        print(listaDecrece, end = ', ')
+    if listaCrece == n:
+        print(listaCrece, end='\n')
+    else:
+        print(listaCrece, end = ', ')
+    
+print(f'\nLista de ({n} a {valorInicial}) de menos uno en menos uno')
+if n >= valorInicial:
+    for listaDecrece in range(n,0,-1):
+        if listaDecrece == valorInicial:
+            print(listaDecrece, end='\n')
+        else:
+            print(listaDecrece, end = ', ') #18 lineas
