54d519510711cf3343438402397f32c112dd2e49
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-07 17:01:15 -0500

    Se termino EstatutoFor2.py

--- semana-2/class-7/A01652612_EstatutoFor2.py
+++ semana-2/class-7/A01652612_EstatutoFor2.py
@@ -2,7 +2,7 @@
 #Rosa Vanessa Palacios Beltran
 #A01652612
 
-# Ejercicio # 1
+# Ejercicio # 1 - proemdio
 
 cant_num = int(input('¿Cuántos números deseas ingresar?: '))
 suma = 0
@@ -16,21 +16,19 @@ promedio = suma/cant_num
 print(f'El promedio de los números es {promedio}')
 
 
-#Ejercicio # 2
-#Cuenta de inversión: 
+#Ejercicio # 2 - Cuenta de inversión: 
 
-#En una cuenta de inversión, a principios del año 2020, 
-# el saldo era de 100,500 pesos. El banco da un rendimiento 
-# de 8% de interés anual, donde cada mes el porcentaje de 
-# interés calculado se acumula al saldo de la cuenta, es 
-# decir, cada mes el saldo de la cuenta al final del mes, 
-# es igual al saldo en ese momento, más el cálculo 
-# del interés mensual. 
+saldo = 100500
+interes = 0.08/12
 
-#En el mes de julio se va hacer un retiro por 90,500 pesos. 
+for meses in range(1,13):
+    if meses == 7: 
+        saldo = saldo - 90500
 
-#Y el mes de octubre se va hacer un depósito por 50,000 pesos. 
+    elif meses == 10: 
+        saldo = saldo + 50000
 
-#Utiliza la función For para calcular y desplegar cuánto va 
-# a ser el saldo de la cuenta al final del año.
+    saldo = saldo + saldo*interes
+    #print(f'{meses}: ${saldo:.2f}')
 
+print(f'El salfo final es de ${saldo:.2f}')
