28cada0e16d735385ebc749d712bcaa4d20960dc
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-07 15:46:37 -0500

    Se quito pedientes y class-5 esta listo para pasar el apunte de la misma

--- semana-2/class-7/A01652612-class-7.py
+++ semana-2/class-7/A01652612-class-7.py
@@ -3,3 +3,28 @@
 #A01652612
 
 #Instrucciones
+
+'''
+#caso comillas
+print(f'Pero \'supuestamente...')
+
+print(f'Pero "supuestamente"...') #error
+
+print(f"Pero 'supuestamente'...")
+'''
+
+#imprimir los numeros del 4 al 40, excepto el 7.
+
+for num in range(4,41):
+    if num == 7:
+        continue
+
+    print(num, end = ' ')
+
+print('\n=======================')
+num = 4
+while num <= 40:
+    if num == 7:
+        continue
+    print(num, end = ' ')
+    num = num + 1
--- semana-2/class-7/A01652612-for-rangos-2.py
+++
@@ -1,5 +0,0 @@
-#Actividad en clase - Estatuto for y los rangos - 1
-#Rosa Vanessa Palacios Beltran
-#A01652612
-
-#Instrucciones
