ca29ed82c4210ab3fc4992b802ca73e6b893d994
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-07 15:58:10 -0500

    Se escribieron las instrucciones de la actividad estatuto for 2

--- semana-2/class-7/A01652612-class-7.py
+++ semana-2/class-7/A01652612-class-7.py
@@ -2,16 +2,10 @@
 #Rosa Vanessa Palacios Beltran
 #A01652612
 
-#Instrucciones
-
-'''
-#caso comillas
+'''caso comillas
 print(f'Pero \'supuestamente...')
-
-print(f'Pero "supuestamente"...') #error
-
-print(f"Pero 'supuestamente'...")
-'''
+print(f'Pero "supuestamente"...') error
+print(f"Pero 'supuestamente'...") '''
 
 #imprimir los numeros del 4 al 40, excepto el 7.
 
@@ -20,11 +14,3 @@ for num in range(4,41):
         continue
 
     print(num, end = ' ')
-
-print('\n=======================')
-num = 4
-while num <= 40:
-    if num == 7:
-        continue
-    print(num, end = ' ')
-    num = num + 1
--- semana-2/class-7/A01652612_EstatutoFor2.py
+++ semana-2/class-7/A01652612_EstatutoFor2.py
@@ -2,4 +2,34 @@
 #Rosa Vanessa Palacios Beltran
 #A01652612
 
-#Instrucciones
+#Instrucciones 
+# Ejercicio # 1
+#Muestra el promedio de todos los números capturados 
+# por el usuario. Primero, pregunta por la cantidad 
+# de números que el usuario desea capturar y luego 
+# pregunta por cada uno de los números y después de 
+# por todos los números; despliega en pantalla el 
+# promedio de todos los números.
+
+
+
+
+
+#Ejercicio # 2
+#Cuenta de inversión: 
+
+#En una cuenta de inversión, a principios del año 2020, 
+# el saldo era de 100,500 pesos. El banco da un rendimiento 
+# de 8% de interés anual, donde cada mes el porcentaje de 
+# interés calculado se acumula al saldo de la cuenta, es 
+# decir, cada mes el saldo de la cuenta al final del mes, 
+# es igual al saldo en ese momento, más el cálculo 
+# del interés mensual. 
+
+#En el mes de julio se va hacer un retiro por 90,500 pesos. 
+
+#Y el mes de octubre se va hacer un depósito por 50,000 pesos. 
+
+#Utiliza la función For para calcular y desplegar cuánto va 
+# a ser el saldo de la cuenta al final del año.
+
