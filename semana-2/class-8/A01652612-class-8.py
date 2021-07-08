95eff9f3f593198f8c21e1950396c2ebdad12310
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-08 16:11:50 -0500

    Ejercicio clase 8 otra froma, tips de imprimir mensaje en python, apuntes class 8 de las versiones de ejercicios de class-8, tarea while archivo en proceso

--- semana-2/class-8/A01652612-class-8.py
+++ semana-2/class-8/A01652612-class-8.py
@@ -2,8 +2,20 @@
 #Rosa Vanessa Palacios Beltran
 #A01652612
 
-#Instrucciones
+#tip tiempo retraso
+'''
+import sys 
+import time
 
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.10)
+    
+var  = 10
+delay_print(f'La variable es {var}')
+'''
 #Formula una pregunta de opcion múltiple al usuario.
 #Mientras la respuesta sea incorrecta, sigue haciendo la misma pregunta.
 
@@ -11,8 +23,15 @@ print('¿Quién escribió el libro de Azteca?')
 print('a)Crevantes \tb)Stepshen King \tc)Gary Jennings \td)Octavio Paz')
 resp = input('Teclea tu respuesta: ')
 
-while resp != 'c':
-    resp = input('Repsuesta incorrecta, vuelve a intentarlo: ')
+intentos = 1
+while intentos < 3:
+    if resp != 'c':
+        resp = input('Repsuesta incorrecta, vuelve a intentarlo: ')
+        intentos = intentos + 1
+    
+    if intentos == 3:
+        print('Te quedaste sin intentos')
 
-print('Respuesta correcta')
-
+    if resp == 'c':
+        print('Respuesta correcta')
+        break
