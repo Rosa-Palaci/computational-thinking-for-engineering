f654bc8ab65bf0db406f810c8ad1ad62a32c5700
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-08 15:51:11 -0500

    Version opcion multiple muy buena y eficaz

--- semana-2/class-8/A01652612-class-8.py
+++ semana-2/class-8/A01652612-class-8.py
@@ -3,3 +3,16 @@
 #A01652612
 
 #Instrucciones
+
+#Formula una pregunta de opcion múltiple al usuario.
+#Mientras la respuesta sea incorrecta, sigue haciendo la misma pregunta.
+
+print('¿Quién escribió el libro de Azteca?')
+print('a)Crevantes \tb)Stepshen King \tc)Gary Jennings \td)Octavio Paz')
+resp = input('Teclea tu respuesta: ')
+
+while resp != 'c':
+    resp = input('Repsuesta incorrecta, vuelve a intentarlo: ')
+
+print('Respuesta correcta')
+
