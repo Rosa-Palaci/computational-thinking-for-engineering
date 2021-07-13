5243d1af27de3b832840c8539dd3eb2bb7609afe
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-12 23:03:28 -0500

    Ejercicio-3 listo

--- semana-3/class-9/ejercicio-3.py
+++ semana-3/class-9/ejercicio-3.py
@@ -5,12 +5,12 @@
 #Ejercicio 3
 
 # Pide al usuario un número entero positivo, 
-# calcula la suma de los números al cuadraado y muestro el resultado.
-'''
+# calcula la suma de los números al cuadrado y muestro el resultado.
 
-num_final = int(input('Introduce el número final: '))
-suma_3 = 0
-for num3 in range(1, num_final + 1):
-    suma_3 = suma_3 + num3**2 #Equivalente a suma_3 += num3
 
-print(f'La suma desde 1^2 hasta {num_final}^2 es {suma} ')'''
+numFinal = int(input('Ingresa un número entero positivo final: '))
+suma = 0
+for num in range(1, numFinal + 1):
+    suma = suma + num**2 
+
+print(f'La suma es desde 1^2 hasta {numFinal}^2 es {suma} ')
