6415bbe2d17e3e460480135d1ce34a0563af0fa5
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-12 23:20:29 -0500

    Ejercicio-4 listo listo

--- semana-3/class-9/ejercicio-4.py
+++ semana-3/class-9/ejercicio-4.py
@@ -17,17 +17,17 @@ while clave != 'X':
     clave = input('\nTeclea la clave del artículo: ')
     if clave == 'A2':
         print('Costo de pantalones: $300')
-        cantA2 = int(input('¿Cuántos articulos deseas adquirir?'))
+        cantA2 = int(input('¿Cuántos articulos deseas adquirir?: '))
         compraFinal = compraFinal + 300*cantA2
     elif clave == 'B39':
         print('Costo de gorra: $150')
-        cantB39 = int(input('¿Cuántos articulos deseas adquirir?'))
+        cantB39 = int(input('¿Cuántos articulos deseas adquirir?: '))
         compraFinal = compraFinal + 150*cantB39
     elif clave == 'L10':
         print('Costo de caletines: $90')
-        cantL10 = int(input('¿Cuántos articulos deseas adquirir?'))
+        cantL10 = int(input('¿Cuántos articulos deseas adquirir?: '))
         compraFinal = compraFinal + 90*cantL10
     elif clave == 'X':
-        print(f'\nCompra finalizada \nTotal a pagar es de {compraFinal}')
+        print(f'\nCompra finalizada \nTotal a pagar es de ${compraFinal}')
     else:
         print('La clave es incorrecta, vuelve a intentarlo')
