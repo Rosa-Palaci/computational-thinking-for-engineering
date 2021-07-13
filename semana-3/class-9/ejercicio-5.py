ac55a4108f2fe6e970d73e2a1cec20b0980b2d2a
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-12 23:37:40 -0500

    Ejercicio 5 listo

--- semana-3/class-9/ejercicio-5.py
+++ semana-3/class-9/ejercicio-5.py
@@ -3,3 +3,18 @@
 #A01652612
 
 #Ejercicio 5
+
+'''
+Muestra una serie de número que empiezan en a y se vayan 
+incrementando de 5 en 5 hasta llegar a b, donde a y b son 
+dos número capturados por el usuario y asume que a siempre 
+es menor que b 
+
+Nota que a y b no necesariamente son múltiplos de 5, y que 
+debe mostrar todos los números que sean menor o igual a b.
+'''
+a = int(input('Introduce \'a\': '))
+b = int(input('Introduce \'b\': '))
+
+for num in range(a, b + 1, 5):
+    print(num, emd = ' ')
