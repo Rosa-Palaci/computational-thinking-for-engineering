914f349e80f627129a0974075ddf4e72f4b00968
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-14 16:26:51 -0500

    Funciones numeros ascedentes

--- semana-3/class-11/mayores-prueba1.py
+++ semana-3/class-11/mayores-prueba1.py
@@ -1,33 +1,36 @@
-import random
-a = random.randint(0, 200)
-b = random.randint(0, 200)
-c = random.randint(0, 200)
+#
+# 
+def orden_asc(a, b, c):
+    if a > b and a > c:
+        mayor = a
+        if b > c:
+            medio = b
+            menor = c
+        else: #Significa que c > b
+            medio = c
+            menor = b
+    elif b > a and b > c:
+        mayor = b
+        if a > c:
+            medio = a
+            menor = c
+        else:
+            medio = c
+            menor = a
+    else: #Significa que 'c' es el mayor
+        mayor = c
+        if a > b:
+            medio = a
+            menor = b
+        else:
+            medio = b
+            menor = a
+    return menor, medio, mayor
 
-print(f'Los números sin ordenar son: {a}, {b} y {c}\n')
-
-if a > b and a > c:
-    mayor = a
-    if b > c:
-        medio = b
-        menor = c
-    else: #Significa que c > b
-        medio = c
-        menor = b
-elif b > a and b > c:
-    mayor = b
-    if a > c:
-        medio = a
-        menor = c
-    else:
-        medio = c
-        menor = a
-else: #Significa que 'c' es el mayor
-    mayor = c
-    if a > b:
-        medio = a
-        menor = b
-    else:
-        medio = b
-        menor = a
-
-print(f'Los números en orden ascendente son: {menor}, {medio}, {mayor}')
+for veces in range(2):
+    a = int(input('Introduce el número 1: '))
+    b = int(input('Introduce el número 2: '))
+    c = int(input('Introduce el número 3: '))
+    menor, medio, mayor = orden_asc(a, b, c)
+    #print(f'Los números sin ordenar son: {a}, {b} y {c}\n')
+    print(f'Los números en orden ascendente son: {menor}, {medio}, {mayor}')
