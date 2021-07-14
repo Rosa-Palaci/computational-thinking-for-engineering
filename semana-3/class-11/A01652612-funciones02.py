b346c78314592a8e375d1a3429bf898b3f7d471e
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-14 17:43:09 -0500

    Se codigo funciones03

--- semana-3/class-11/A01652612-funciones02.py
+++ semana-3/class-11/A01652612-funciones02.py
@@ -9,29 +9,3 @@ Define una función que reciba el lado de un cubo y calcule su área.
 Utiliza esa función para calcular el área de tres cubos distintos 
 (el lado de cada cubo lo proporciona el usuario).
 '''
-import math
-
-def ecuacionCuad(a, b, c):
-
-    if b**2 - 4 *(a*c) < 0:
-        x1 = math.nan
-        x2 = math.nan
-    else: 
-        x1 = (-b + math.sqrt (b**2 - 4 * a*c)) / (2*a)
-        x2 = (-b - math.sqrt (b**2 - 4 * a*c)) / (2*a)
-    
-    return x1, x2
-
-a = int(input('Introduce a: '))
-b = int(input('Introduce b: '))
-c = int(input('Introduce c: '))
-
-x1, x2 = ecuacionCuad(a, b, c)
-print(f'Las soluciones son: x1={x1} x2={x2} ')
-
-'''
-Introduce a - 1
-Introduce b - 8
-introduce c - 15
-Las soluciones son x1= 3.0 x2=-5.0
-'''
--- semana-3/class-11/A01652612-funciones03.py
+++ semana-3/class-11/A01652612-funciones03.py
@@ -15,3 +15,26 @@ deberán regresar el valor NaN (not a number), representado en Python como math.
 Utiliza esa función para calcular la solución de dos ecuaciones cuadráticas distintas 
 (los coeficientes de ambas ecuaciones son proporcionadas por el usuario).
 '''
+import math
+
+print('\nIngresa valores para los coeficientes de a, b y c\n \nPara una ecuación cuadrática de la forma:\n ax^2 + bx + c = 0\n')
+
+def ecuacionCuadratica(a, b, c):
+
+    if b**2 - 4 *(a*c) < 0:
+        x1 = math.nan
+        x2 = math.nan
+    else: 
+        x1 = (-b + math.sqrt (b**2 - 4 * a*c)) / (2*a)
+        x2 = (-b - math.sqrt (b**2 - 4 * a*c)) / (2*a)
+    
+    return x1, x2
+
+a = int(input('Digite valor de a: '))
+b = int(input('Digite valor de b: '))
+c = int(input('Digite valor de c: '))
+print(f'\n{a}x^2 + {b}x + {c} = 0\n')
+
+x1, x2 = ecuacionCuadratica(a, b, c)
+
+print(f'Las soluciones para\n x1={x1} | x2={x2} ')
