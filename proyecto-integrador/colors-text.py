6e890a3948a937b23f9bcdd1346bcf14bc213f6e
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-15 18:02:56 -0500

    Se agrego texto colorful para titulo

---
+++ proyecto-integrador/colors-text.py
@@ -0,0 +1,15 @@
+
+import colorama
+from colorama import Back, Fore, Style, init
+
+colorama.init(autoreset=True)
+print('\n')
+print(Fore.GREEN + "M" + Fore.LIGHTGREEN_EX + "A" + Fore.LIGHTYELLOW_EX + "G" + Fore.YELLOW + "I" + Fore.LIGHTRED_EX + "C " + Fore.RED + "M" + Fore.MAGENTA + "A" + Fore.LIGHTMAGENTA_EX + "T" + Fore.LIGHTBLUE_EX + "H" + Fore.BLUE + " G" + Fore.CYAN + "A" + Fore.LIGHTCYAN_EX + "M" + Fore.WHITE + "E")
+print(Fore.LIGHTGREEN_EX + "M" + Fore.GREEN + "A" + Fore.BLUE + "G" + Fore.LIGHTBLUE_EX + "I" + Fore.LIGHTMAGENTA_EX + "C " + Fore.MAGENTA + "M" + Fore.RED + "A" + Fore.LIGHTRED_EX + "T" + Fore.YELLOW + "H" + Fore.LIGHTYELLOW_EX + " G" + Fore.WHITE + "A" + Fore.LIGHTWHITE_EX + "M" + Fore.LIGHTBLACK_EX + "E")
+print(f"{Fore.BLACK}{Back.RED}M{Fore.BLACK}{Back.RED}A{Fore.BLACK}{Back.RED}G{Fore.BLACK}{Back.RED}I{Fore.BLACK}{Back.RED}C {Fore.BLACK}{Back.RED}M{Fore.BLACK}{Back.RED}A{Fore.BLACK}{Back.RED}T{Fore.BLACK}{Back.RED}H {Fore.BLACK}{Back.RED}G{Fore.BLACK}{Back.RED}A{Fore.BLACK}{Back.RED}M{Fore.BLACK}{Back.RED}E")
+print(f"{Fore.BLACK}{Back.RED}M{Fore.BLACK}{Back.LIGHTRED_EX}A{Fore.BLACK}{Back.YELLOW}G{Fore.BLACK}{Back.LIGHTYELLOW_EX}I{Fore.BLACK}{Back.LIGHTGREEN_EX}C {Fore.BLACK}{Back.GREEN}M{Fore.BLACK}{Back.LIGHTCYAN_EX}A{Fore.BLACK}{Back.CYAN}T{Fore.BLACK}{Back.LIGHTBLUE_EX}H {Fore.BLACK}{Back.BLUE}G{Fore.BLACK}{Back.LIGHTMAGENTA_EX}A{Fore.BLACK}{Back.MAGENTA}M{Fore.BLACK}{Back.MAGENTA}E")
+print('\n')
+print(f"\n{Fore.BLACK}{Back.RED}M{Fore.BLACK}{Back.LIGHTRED_EX}A{Fore.BLACK}{Back.YELLOW}G{Fore.BLACK}{Back.LIGHTYELLOW_EX}I{Fore.BLACK}{Back.LIGHTGREEN_EX}C {Fore.BLACK}{Back.GREEN}M{Fore.BLACK}{Back.LIGHTCYAN_EX}A{Fore.BLACK}{Back.CYAN}T{Fore.BLACK}{Back.LIGHTBLUE_EX}H {Fore.BLACK}{Back.BLUE}G{Fore.BLACK}{Back.LIGHTMAGENTA_EX}A{Fore.BLACK}{Back.MAGENTA}M{Fore.BLACK}{Back.MAGENTA}E"'\n')
+print(f"\n{Fore.RED}M{Fore.LIGHTRED_EX}A{Fore.YELLOW}G{Fore.LIGHTYELLOW_EX}I{Fore.LIGHTGREEN_EX}C {Fore.GREEN}M{Fore.LIGHTCYAN_EX}A{Fore.CYAN}T{Fore.LIGHTBLUE_EX}H {Fore.BLUE}G{Fore.LIGHTMAGENTA_EX}A{Fore.MAGENTA}M{Fore.MAGENTA}E"'\n')
+
+#MAGIC MATH GAME
---
+++ proyecto-integrador/segunda-entrega.py
@@ -0,0 +1,23 @@
+#Proyecto integrador: Segunda entrega
+
+#Realiza la codificación inicial de tu proyecto. Asegúrate de que usas los siguientes elementos:
+# Operaciones aritméticas
+# Condicionales
+# Ciclos
+# Funciones
+# Listas
+
+#MAGIC MATH GAME
+def main():
+    import colorama
+    from colorama import Back, Fore, Style, init
+
+    colorama.init(autoreset=True)
+
+    #print(f"\n{Fore.BLACK}{Back.RED}M{Fore.BLACK}{Back.LIGHTRED_EX}A{Fore.BLACK}{Back.YELLOW}G{Fore.BLACK}{Back.LIGHTYELLOW_EX}I{Fore.BLACK}{Back.LIGHTGREEN_EX}C {Fore.BLACK}{Back.GREEN}M{Fore.BLACK}{Back.LIGHTCYAN_EX}A{Fore.BLACK}{Back.CYAN}T{Fore.BLACK}{Back.LIGHTBLUE_EX}H {Fore.BLACK}{Back.BLUE}G{Fore.BLACK}{Back.LIGHTMAGENTA_EX}A{Fore.BLACK}{Back.MAGENTA}M{Fore.BLACK}{Back.MAGENTA}E"'\n')
+    print(f"\n{Fore.RED}M{Fore.LIGHTRED_EX}A{Fore.YELLOW}G{Fore.LIGHTYELLOW_EX}I{Fore.LIGHTGREEN_EX}C {Fore.GREEN}M{Fore.LIGHTCYAN_EX}A{Fore.CYAN}T{Fore.LIGHTBLUE_EX}H {Fore.BLUE}G{Fore.LIGHTMAGENTA_EX}A{Fore.MAGENTA}M{Fore.MAGENTA}E"'\n')
+
+
+
+
+main()
